fa46289a79d3a80dc3d68863d06effddb75b91d7
SunandhaK <chinnik11294@gmail.com>
2021-04-21 11:23:57 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class Array {
+
+	public static void main(String[] args)
+	{
+		float one[] = new float[10];		 
+		one[0] = 10.2f;
+		one[1] = 12.2f;
+		one[2] = 15.6f;
+		one[3] = 17.2f;
+		one[4] = 20.0f;
+		one[5] = 25.4f;
+		one[6] = 28.2f;
+		one[7] = 11.2f;
+		one[8] = 18.5f;
+		one[9] = 14.3f;
+	for (int i=0; i<one.length-1;i++)
+	{
+		System.out.println(one[i]);
+	}
+	}
+
+}
